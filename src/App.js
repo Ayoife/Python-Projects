f647365451ca49d76a51f0a7416d974d9a9af091
Ayoife <ayindeoladeindeayoife@gmail.com>
2023-09-11 04:45:11 +0100

    Added Test.js to /src and added new line to card

--- src/App.js
+++ src/App.js
@@ -32,7 +32,8 @@ function About() {
             <p>
                 I am a frontend developer with a particular interest in making
                 things simple and automating daily tasks. I try to keep up with security
-                and best practices, and I'm always looking for new things to learn ;)
+                and best practices, and I'm always looking for new things to learn ;
+                Yh that's me!)
             </p>
         </div>)
 
